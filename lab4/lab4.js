e1aeec60c3e2222a396fc3082c28f9cb9ded0ad5
kseniy-san <ksyusankoa@yandex.ru>
2025-05-23 15:04:04 +0300

    end

---
+++ lab4/lab4.js
@@ -0,0 +1,21 @@
+"use strict";
+class Book {
+    /**
+     * Создает экземпляр книги.
+     * @param {string} title - Название книги.
+     * @param {number} pubYear - Год публикации.
+     * @param {number} price - Цена книги.
+     */
+    constructor(title, pubYear, price) {
+        this.title = title;
+        this.pubYear = pubYear;
+        this.price = price;
+    }
+
+    show() {
+        console.log(`Название: ${this.title}, Цена: ${this.price} руб`);
+    }
+}
+
+const myBook = new Book("Мы", 1920, 300);
+myBook.show(); 
