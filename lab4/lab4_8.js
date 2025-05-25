1901e4b8c56f0503f4dd8b4b9f584a2d8a2f63dd
kseniy-san <ksyusankoa@yandex.ru>
2025-05-25 23:18:39 +0300

    8

---
+++ lab4/lab4_8.js
@@ -0,0 +1,13 @@
+function formatDate(date) {
+  const padZero = (n) => n.toString().padStart(2, '0');
+  
+  const day = padZero(date.getDate());
+  const month = padZero(date.getMonth() + 1); 
+  const year = date.getFullYear().toString().slice(-2); 
+  
+  return `${day}.${month}.${year}`;
+}
+
+console.log(formatDate(new Date(2025, 9, 5))); 
+const today = new Date();
+console.log(formatDate(today));
