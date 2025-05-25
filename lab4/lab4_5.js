445e24ea85fe11305d21b9d43a9b89c96fc69a20
kseniy-san <ksyusankoa@yandex.ru>
2025-05-25 22:56:59 +0300

    5

---
+++ lab4/lab4_5.js
@@ -0,0 +1,33 @@
+let obj = {
+  className: 'open menu',
+  
+  /**
+   * Добавляет класс в список, если его там нет.
+   * @param {string} cls - Добавляемый класс.
+   * @returns {Object} - Возвращает сам объект.
+   */
+  addClass(cls) {
+    const classes = this.className.split(' ');
+    if (!classes.includes(cls)) {
+      classes.push(cls);
+    }
+    this.className = classes.join(' ');
+    return this;
+  },
+  
+  /**
+   * Удаляет класс из списка, если он есть.
+   * @param {string} cls - Удаляемый класс.
+   * @returns {Object} - Возвращает сам объект.
+   */
+  removeClass(cls) {
+    const classes = this.className.split(' ').filter(c => c !== '' && c !== cls);
+    this.className = classes.join(' ');
+    return this;
+  }
+};
+
+console.log(obj.className);
+
+obj.addClass('new').removeClass('open');
+console.log(obj.className); // "menu new"
